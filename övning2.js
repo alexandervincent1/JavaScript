4e17afe098e1000e493c6c802e5e00291d07c448
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2025-08-25 14:50:51 +0200

    bytt namn på html

--- övning2.js
+++ övning2.js
@@ -1,17 +1,18 @@
-// //let Age = 19;
-// const Pi = 3.14;
-// let Student = true;
-// console.log(Age);
-// console.log(Pi);
-// console.log(Student);
+// // //let Age = 19;
+// // const Pi = 3.14;
+// // let Student = true;
+// // console.log(Age);
+// // console.log(Pi);
+// // console.log(Student);
 
 
 
-// for (let x = 15; x >= 0; x--)
-//     console.log(x);
-const Färger = ["Röd","Grön","Blå"];
-console.log(Färger);
-// for (let y = 0; y <= Färger.length; y++) {
-//     console.log(Färger[y]);
-//     console.log("Hej")
+// // for (let x = 15; x >= 0; x--)
+// //     console.log(x);
+
+// let Färger = ["Röd","Grön","Blå"];
+// let FärgerLängd = Färger.length;
+// for (let i = 0; i< FärgerLängd; i++) {
+//     console.log(Färger[i]);
 // }
+
