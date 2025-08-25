97ab73841be78445523077497477fc5202e29de0
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2025-08-25 14:50:03 +0200

    first commit

---
+++ övning2.js
@@ -0,0 +1,17 @@
+// //let Age = 19;
+// const Pi = 3.14;
+// let Student = true;
+// console.log(Age);
+// console.log(Pi);
+// console.log(Student);
+
+
+
+// for (let x = 15; x >= 0; x--)
+//     console.log(x);
+const Färger = ["Röd","Grön","Blå"];
+console.log(Färger);
+// for (let y = 0; y <= Färger.length; y++) {
+//     console.log(Färger[y]);
+//     console.log("Hej")
+// }
