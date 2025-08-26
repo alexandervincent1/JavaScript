fb9d8336a7a309d85207e06029ddab9ce2a39b15
alexandervincent1 <alexander.vincent@elev.ga.ntig.se>
2025-08-26 14:38:50 +0200

    övning2 klart

---
+++ övning2/övning2.js
@@ -0,0 +1,18 @@
+let Age = 19;
+const Pi = 3.14;
+let Student = true;
+console.log(Age);
+console.log(Pi);
+console.log(Student);
+
+
+
+for (let x = 15; x >= 0; x--)
+    console.log(x);
+
+let Färger = ["Röd","Grön","Blå"];
+let FärgerLängd = Färger.length;
+for (let i = 0; i< FärgerLängd; i++) {
+    console.log(Färger[i]);
+}
+
---
+++ övning2/övning2lång.js
@@ -0,0 +1,40 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+function AddTask() {
+    if (inputBox.value === '') {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    RenderList()
+}
+
+
+
+listContainer.addEventListener('click', function (e) {
+    if (e.target.tagName === 'LI') {
+        e.target.classList.toggle('checked');
+        RenderList()
+    }
+    else if (e.target.tagName === 'SPAN') {
+        e.target.parentElement.remove();
+        RenderList()
+    }
+}, false);
+
+
+function RenderList() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function GetList() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+GetList()
